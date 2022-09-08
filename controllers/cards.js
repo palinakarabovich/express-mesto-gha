1393fe7a21aca92799c1c5600970af3285352c87
Palina Karabovich <polina.korobovich@yandex.by>
2022-09-08 14:43:01 +0200

    error fixed

--- controllers/cards.js
+++ controllers/cards.js
@@ -71,7 +71,7 @@ const likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => {
       if (card === null) {
-        res.status(400).send({ message: 'Карточка с указанным _id не найдена' });
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
       } else {
         const {
           likes,
@@ -91,6 +91,8 @@ const likeCard = (req, res) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
       } else { res.status(500).send({ message: `Произошла ошибка ${err.name}` }); }
     });
 };
@@ -99,7 +101,7 @@ const dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => {
       if (card === null) {
-        res.status(400).send({ message: 'Карточка с указанным _id не найдена' });
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
       } else {
         const {
           likes,
